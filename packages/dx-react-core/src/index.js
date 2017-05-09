30faa00d518c144c8ba3936850625dc27c735634
HappyCreater <happycreater1006@gmail.com>
2017-05-09 07:18:49 +0000

    Prepare GitHub pages build tasks and demos

--- packages/dx-react-core/src/index.js
+++ packages/dx-react-core/src/index.js
@@ -1,6 +1,1 @@
-export { PluginHost } from './plugged/host';
-export { Action } from './plugged/action';
-export { Getter } from './plugged/getter';
-export { Watcher } from './plugged/watcher';
-export { Template } from './plugged/template';
-export { TemplatePlaceholder } from './plugged/template-placeholder';
+export * from './plugged';
